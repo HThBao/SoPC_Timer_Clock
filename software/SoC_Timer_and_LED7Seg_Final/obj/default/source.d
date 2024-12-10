d551376888ddf71bc0ae37cb910bce824b014882
HThBao <137778579+HThBao@users.noreply.github.com>
2024-12-10 14:22:31 +0700

    Add files via upload

---
+++ software/SoC_Timer_and_LED7Seg_Final/obj/default/source.d
@@ -0,0 +1,25 @@
+obj/default/source.o: source.c \
+ ../SoC_Timer_and_LED7Seg_Final_bsp//HAL/inc/io.h \
+ ../SoC_Timer_and_LED7Seg_Final_bsp//HAL/inc/alt_types.h \
+ ../SoC_Timer_and_LED7Seg_Final_bsp/system.h \
+ ../SoC_Timer_and_LED7Seg_Final_bsp/linker.h \
+ ../SoC_Timer_and_LED7Seg_Final_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../SoC_Timer_and_LED7Seg_Final_bsp//HAL/inc/sys/alt_irq.h \
+ ../SoC_Timer_and_LED7Seg_Final_bsp//HAL/inc/nios2.h \
+ ../SoC_Timer_and_LED7Seg_Final_bsp//HAL/inc/alt_types.h
+
+../SoC_Timer_and_LED7Seg_Final_bsp//HAL/inc/io.h:
+
+../SoC_Timer_and_LED7Seg_Final_bsp//HAL/inc/alt_types.h:
+
+../SoC_Timer_and_LED7Seg_Final_bsp/system.h:
+
+../SoC_Timer_and_LED7Seg_Final_bsp/linker.h:
+
+../SoC_Timer_and_LED7Seg_Final_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../SoC_Timer_and_LED7Seg_Final_bsp//HAL/inc/sys/alt_irq.h:
+
+../SoC_Timer_and_LED7Seg_Final_bsp//HAL/inc/nios2.h:
+
+../SoC_Timer_and_LED7Seg_Final_bsp//HAL/inc/alt_types.h:
